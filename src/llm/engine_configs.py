e8ff227deb5f80b94fa52112092631680f8af47b
Shayan Talaei <62253512+ShayanTalaei@users.noreply.github.com>
2025-05-26 11:20:10 -0700

    Update engine_configs.py

--- src/llm/engine_configs.py
+++ src/llm/engine_configs.py
@@ -49,22 +49,6 @@ ENGINE_CONFIGS: Dict[str, Dict[str, Any]] = {
         "constructor": VertexAI,
         "params": {"model": "gemini-1.5-pro-002", "temperature": 0, "safety_settings": safety_settings}
     },
-    "gemini-1.5-flash":{
-        "constructor": VertexAI,
-        "params": {"model": "gemini-1.5-flash", "temperature": 0, "safety_settings": safety_settings}
-    },
-    "picker_gemini_model": {
-        "constructor": VertexAI,
-        "params": {"model": "projects/613565144741/locations/us-central1/endpoints/7618015791069265920", "temperature": 0, "safety_settings": safety_settings}
-    },
-    "gemini-1.5-pro-text2sql": {
-        "constructor": VertexAI,
-        "params": {"model": "projects/618488765595/locations/us-central1/endpoints/1743594544210903040", "temperature": 0, "safety_settings": safety_settings}
-    },
-    "cot_picker": {
-        "constructor": VertexAI,
-        "params": {"model": "projects/243839366443/locations/us-central1/endpoints/2772315215344173056", "temperature": 0, "safety_settings": safety_settings}
-    },
     "gpt-3.5-turbo-0125": {
         "constructor": ChatOpenAI,
         "params": {"model": "gpt-3.5-turbo-0125", "temperature": 0}
@@ -97,55 +81,6 @@ ENGINE_CONFIGS: Dict[str, Dict[str, Any]] = {
         "constructor": ChatAnthropic,
         "params": {"model": "claude-3-opus-20240229", "temperature": 0}
     },
-    # "finetuned_nl2sql": {
-    #     "constructor": ChatOpenAI,
-    #     "params": {
-    #         "model": "AI4DS/NL2SQL_DeepSeek_33B",
-    #         "openai_api_key": "EMPTY",
-    #         "openai_api_base": "/v1",
-    #         "max_tokens": 400,
-    #         "temperature": 0,
-    #         "stop": ["```\n", ";"]
-    #     }
-    # },
-    "finetuned_nl2sql": {
-        "constructor": ChatOpenAI,
-        "params": {
-            "model": "ft:gpt-4o-mini-2024-07-18:stanford-university::9p4f6Z4W",
-            "max_tokens": 400,
-            "temperature": 0,
-            "stop": ["```\n", ";"]
-        }
-    },
-    "column_selection_finetuning": {
-        "constructor": ChatOpenAI,
-        "params": {
-            "model": "ft:gpt-4o-mini-2024-07-18:stanford-university::9t1Gcj6Y:ckpt-step-1511",
-            "max_tokens": 1000,
-            "temperature": 0,
-            "stop": [";"]
-        }
-    },
-    # "finetuned_nl2sql_cot": {
-    #     "constructor": ChatOpenAI,
-    #     "params": {
-    #         "model": "AI4DS/deepseek-cot",
-    #         "openai_api_key": "EMPTY",
-    #         "openai_api_base": "/v1",
-    #         "max_tokens": 1000,
-    #         "temperature": 0,
-    #         "stop": [";"]
-    #     }
-    # },
-    # "finetuned_nl2sql_cot": {
-    #     "constructor": ChatOpenAI,
-    #     "params": {
-    #         "model": "ft:gpt-4o-mini-2024-07-18:stanford-university::9oKvRYet",
-    #         "max_tokens": 1000,
-    #         "temperature": 0,
-    #         "stop": [";"]
-    #     }
-    # },
     "meta-llama/Meta-Llama-3-70B-Instruct": {
         "constructor": ChatOpenAI,
         "params": {
